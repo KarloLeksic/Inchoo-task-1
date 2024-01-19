0592765f49b6d7818016c5b7f419514346ff5a13
Karlo Leksic <lekxka@gmail.com>
2024-01-19 09:25:55 +0100

    fixed navbar so that links are not repeated

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
-const mobileMenuEl = document.querySelector('#mobile-menu');
+const mobileMenuEl = document.querySelector('#menu');
 const openMenuBtn = document.querySelector('#open-menu-icon');
 const closeMenuBtn = document.querySelector('#close-menu-icon');
 const bodyEl = document.querySelector('body');
+const overlay = document.querySelector('#overlay');
 
 // Open mobile menu
 openMenuBtn.addEventListener('click', () => {
     mobileMenuEl.classList.add('shown');
     bodyEl.classList.add('nav-open');
+    overlay.classList.add('active');
+
 });
 
 closeMenuBtn.addEventListener('click', () => {
     mobileMenuEl.classList.remove('shown');
     bodyEl.classList.remove('nav-open');
+    overlay.classList.remove('active');
 });
