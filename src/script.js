60bc804fcab096453a76e00a4166be0fad56cc41
Karlo Leksic <lekxka@gmail.com>
2024-01-19 10:07:28 +0100

    added swiper for mobile

--- src/script.js
+++ src/script.js
@@ -17,3 +17,24 @@ closeMenuBtn.addEventListener('click', () => {
     bodyEl.classList.remove('nav-open');
     overlay.classList.remove('active');
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
